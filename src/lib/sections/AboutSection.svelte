b938ce14876cad15a056314532dad88f110aa17f
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 16:41:18 -0600

    + About Section

---
+++ src/lib/sections/AboutSection.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import SectionBase from '$components/SectionBase.svelte';
+	import { basics } from '$cv';
+
+	const { summary } = basics;
+</script>
+
+<SectionBase title="Sobre mí">
+	<p>
+		{@html summary}
+	</p>
+</SectionBase>
+
+<style lang="postcss">
+</style>
