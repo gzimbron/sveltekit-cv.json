50d7bd42e74d31b0e7a7835b2a472a454aab9f06
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 17:13:04 -0600

    layout update css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 
 <style lang="postcss">
 	main {
-		@apply mx-auto min-h-screen max-w-3xl px-10 py-12;
-		@apply flex flex-col gap-4 border-x-2;
+		@apply mx-auto min-h-screen max-w-3xl px-5 py-12 sm:px-10;
+		@apply flex flex-col gap-4 md:border-x-2;
 	}
 </style>
