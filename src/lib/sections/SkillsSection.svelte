4cc94e6ff6e3bef80ddb78db8b25747c6b4b403e
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 18:14:09 -0600

    skills

--- src/lib/sections/SkillsSection.svelte
+++ src/lib/sections/SkillsSection.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import SectionBase from '$components/SectionBase.svelte';
+	import { skills } from '$cv';
 </script>
 
-<SectionBase title="Proyectos">...</SectionBase>
+<SectionBase title="Habilidades">
+	<ul>
+		{#each skills as { name, level }}
+			<li class="badge badge-outline badge-sm text-primary sm:badge-md">
+				{name}
+				<progress class="progress progress-primary w-20" value={level} max="100"></progress>
+			</li>
+		{/each}
+	</ul>
+</SectionBase>
 
 <style lang="postcss">
+	ul {
+		@apply flex flex-wrap gap-2;
+	}
+	li {
+		@apply flex gap-2;
+	}
 </style>
