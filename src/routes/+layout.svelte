c4a55586d65b26a3224b044d57c7c7485ea36e5b
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 19:15:33 -0600

    quitando borde

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,6 @@
 <style lang="postcss">
 	main {
 		@apply mx-auto min-h-screen max-w-3xl px-5 py-12 sm:px-10;
-		@apply flex flex-col gap-4 md:border-x-2;
+		@apply flex flex-col gap-4;
 	}
 </style>
