1f823c43afbb2b2f7a8a80024ac5762df17ec22d
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 19:51:34 -0600

    arreglos de hero

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,9 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-
+const cssnesting = require('@tailwindcss/nesting');
 const config = {
 	plugins: [
+		cssnesting,
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
