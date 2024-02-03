715cbc3d40ed15e23eba8adcfbf20be33cd07451
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-02 19:18:35 -0600

    adaptador netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +6,7 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
+		adapter: adapter({}),
 		alias: {
 			$appTheme: './src/apptheme.css',
 			$appCss: './src/app.postcss',
